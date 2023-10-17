f0d57dc41d256d3f8e37cd03a39f2a0556fc1b26
Zehirrrrr <cmodyb@yahoo.com>
2023-10-17 03:41:57 -0500

    Quit to main menu when player dies

--- CS-4423 Final Project/Assets/Scripts/Player.cs
+++ CS-4423 Final Project/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -42,6 +43,7 @@ public class Player : MonoBehaviour
     void Die()
     {
         Debug.Log("Player died");
+        SceneManager.LoadScene("MainMenu");
     }
 
     void Stun()
